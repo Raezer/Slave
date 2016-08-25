cfc81dd94b1f8d2638c379dbbb03dd71b31d4efb
Raezer <starosele@gmail.com>
2016-08-25 10:02:02 +0300

    added more checks

--- src/AdisRecv.cpp
+++ src/AdisRecv.cpp
@@ -3,79 +3,149 @@
 #include "stm32f4xx_spi.h"
 int cmdHistory[100];
 int cmdHandled[100];
-	int buf;
-	int ans;
-	int cmdHandledN=0,i=0;
-SPISlave device(PB_5, PA_6, PA_5,PA_4); 
-//SPISlave(PinName mosi, PinName miso, PinName sclk, PinName ssel);
+	int buf,pageNum,ans,cmdHandledN=0,i=0;
+
+	SPISlave device(PB_5, PA_6, PA_5,PA_4); //mosi,miso,sclk,ssel
+
 void init(){
-	
 	device.format(SPI_BITS_PER_SENDING, SPI_MODE);
 	device.frequency(SPI_STARTING_FREQ);
   device.reply(0x00); 
 }
-int pageNum;
+int checkREG(int buf)
+{
+	ans=0x00;
+	
+			 if (buf ==0x8003)
+			 {
+			 pageNum=3;
+				
+			 }
+			 if (buf ==0x8000)
+			 {
+			 pageNum=0;
+				
+			 }
+			//selftest
+			if (buf == 0x0A00)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x00;
+				cmdHandledN++;
+				
+			 }
+			//mode
+			if (buf ==0x5000)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x000a;
+				cmdHandledN++;
+				
+			 }
+			//whoami
+			if (buf ==0x7E00)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x4060;
+				cmdHandledN++;
+				
+			 }
+		
+			if (buf ==ADISREG_X_ACCL_OUT)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x30D4;
+				cmdHandledN++;
+				
+			 }
+		
+		  if (buf ==ADISREG_Y_ACCL_OUT)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0010;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_Z_ACCL_OUT)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0030;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_X_GYRO_OUT)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x57E4;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_Y_GYRO_OUT)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0010;
+				cmdHandledN++;
+				
+			 }
+		   if (buf ==ADISREG_Z_GYRO_OUT)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0030;
+				cmdHandledN++;
+				
+			 }
+		 if (buf ==ADISREG_X_MAGN_OUT)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x7FFF;
+				cmdHandledN++;
+				
+			 }
+		   if (buf ==ADISREG_Y_MAGN_OUT)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0010;
+				cmdHandledN++;
+				
+			 }
+		   if (buf ==ADISREG_Z_MAGN_OUT)
+       {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0030;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_BAROM_OUT)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x7FFF;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_BAROM_LOW)
+		   {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0001;
+				cmdHandledN++;
+				
+			 }
+		  if (buf ==ADISREG_TEMP_OUT)
+			 {
+				cmdHandled[cmdHandledN]=buf;
+				ans=0x0000;
+				cmdHandledN++;
+				
+			 }
+	 
+			
+			//prod_id=0x4060
+		 cmdHistory[i]=buf;
+		 i++;	
+	return ans;
+}
 void adisRecieve(){
 	 if(device.receive()) {
      buf = device.read();   // Read byte from master
-            //переключение страниц, ждем 0х8003
-						//8280 8300 softreset
-		 
-		 if (buf==0x8003)
-			{
-			 pageNum=3;
-			}
-			 if (buf==0x8000)
-			{
-			 pageNum=0;
-			}
-			//selftest
-			if (buf==0x0A00)
-			{
-				cmdHandled[cmdHandledN]=buf;
-				buf=0x00;
-				cmdHandledN++;
-			}
-			//mode
-			if (buf==0x5000)
-			{
-				cmdHandled[cmdHandledN]=buf;
-				buf=0x000a;
-				cmdHandledN++;
-			}
-			//whoami
-			if (buf==0x7E00)
-			{
-				cmdHandled[cmdHandledN]=buf;
-				buf=0x4060;
-				cmdHandledN++;
-			}
-			//prod_id=0x4060
-		 cmdHistory[i]=buf;
-		 device.reply(buf);
-		 i++;	
-		
+     device.reply(checkREG(buf));
 		 }
 }
-
-//     a_pageNum =3;
-//ADISREG_PAGE_ID=0x00
-//_write_spi(_compose_write_cmd(ADISREG_PAGE_ID, a_pageNum));
-//uint16_t adis16480_spi::_compose_write_cmd(uint8_t a_register, uint8_t a_data) {
-//	return (static_cast<uint16_t>(a_register | ADIS_WRITE_FLAG)<<8) | a_data;
-//	}
-
-//_write_uint16_cmd(ADISREG_GLOB_CMD, FLAG_GLOB_CMD_SOFTWARE_RESET);
-
-//void adis16480_spi::_write_uint16_cmd(uint8_t a_reg, uint16_t a_cmd) {
-//	_write_spi(_compose_write_cmd(a_reg, a_cmd & 0xff));
-//	_write_spi(_compose_write_cmd(a_reg+1, a_cmd >> 8));
-//	}	
-
-//	int16_t adis16480_spi::_write_spi(uint16_t a) {
-//	select();
-//	int16_t res = spi.write(a);
-//	deselect();
-//	wait_us(STALL_PERIOD_MCS);	
-//	return res;
-//	}
